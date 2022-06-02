8187ebebb8ddfa4c425630864fe3e50276ffe311
auryshka <97383046+auryshka@users.noreply.github.com>
2022-06-02 21:12:37 +0300

    2022-06-01

--- src/main/java/lt/bit/products/ui/service/SupplierService.java
+++ src/main/java/lt/bit/products/ui/service/SupplierService.java
@@ -1,15 +1,12 @@
 package lt.bit.products.ui.service;
 
 import java.util.List;
+import java.util.UUID;
 import lt.bit.products.ui.model.Supplier;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional
-public class SupplierService {
+public interface SupplierService {
 
-  public List<Supplier> getSuppliers() {
-    return List.of();
-  }
+  List<Supplier> getSuppliers();
+
+  Supplier getSupplier(UUID id);
 }
---
+++ src/main/java/lt/bit/products/ui/service/SupplierServiceH2.java
@@ -0,0 +1,34 @@
+package lt.bit.products.ui.service;
+
+import java.util.List;
+import java.util.UUID;
+import lt.bit.products.ui.model.Supplier;
+import lt.bit.products.ui.service.domain.SupplierEntity;
+import lt.bit.products.ui.service.domain.SupplierRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class SupplierServiceH2 implements SupplierService {
+
+  private final SupplierRepository repository;
+  private final ModelMapper mapper;
+
+  public SupplierServiceH2(SupplierRepository repository, ModelMapper mapper) {
+    this.repository = repository;
+    this.mapper = mapper;
+  }
+
+  public List<Supplier> getSuppliers() {
+    List<SupplierEntity> suppliers = repository.findAll();
+    // @formatter:off
+    return mapper.map(suppliers, new TypeToken<List<Supplier>>() {}.getType());
+    // @formatter:on
+  }
+
+  @Override
+  public Supplier getSupplier(UUID id) {
+    return repository.findById(id).map(s -> mapper.map(s, Supplier.class)).orElseThrow();
+  }
+}
